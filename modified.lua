4a474fea68ba508f3a722e18b9c97784fc95ab9f
Jonathan Torres <JonSuperKnife@gmail.com>
2024-01-13 19:05:06 -0800

    Update modified.lua

--- modified.lua
+++ modified.lua
@@ -79,8 +79,8 @@ local function processListingInfo(uid, gems, item, version, shiny, amount, bough
         ['embeds'] = {
             {
 		["author"] = {
-			["name"] = "Boor Sniper 🌚",
-			["icon_url"] = "https://cdn.discordapp.com/attachments/1149218291957637132/1190527382583525416/new-moon-face_1f31a.png?ex=65a22006&is=658fab06&hm=55f8900eef039709c8e57c96702f8fb7df520333ec6510a81c31fc746193fbf2&",
+			["name"] = "Super Sniper ⭐",
+			["icon_url"] = "https://discord.com/assets/2aa5a1ebb3d1b1f6169d.svg",
 		},
                 ['title'] = snipeMessage,
                 ["color"] = webcolor,
