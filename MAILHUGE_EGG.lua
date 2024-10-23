f3c6d80d024c5441f1f6d651f92059af6cca6f7a
Jonathan Torres <JonSuperKnife@gmail.com>
2024-10-23 13:25:25 -0700

    Update MAILHUGE_EGG.lua
    Revert Pumpkin Change because it did not work.

--- MAILHUGE_EGG.lua
+++ MAILHUGE_EGG.lua
@@ -3,7 +3,6 @@ getgenv().Mailing = {
     Mail_Items = {
         {Class = "Lootbox", Item = "Hype Egg", pt = nil, sh = nil, tn = nil},
         {Class = "Egg", Item = "Huge Machine Egg 4", pt = nil, sh = nil, tn = nil},
-        {Class = "Misc", Item = "Pumpkin", pt = nil, sh = nil, tn = nil, MinAmount = 500 }
     }
 }
 
@@ -23,7 +22,7 @@ Mail_Item = function()
                 ConfigMatch = true
             else
                 for _, CustomItem in pairs(getgenv().Mailing.Mail_Items) do
-                    if CustomItem.Item == v.id and CustomItem.pt == v.pt and CustomItem.sh == v.sh and CustomItem.tn == v.tn and CustomItem.Class == Class and (CustomItem.MinAmount or 0) >= (v._am or 1) then
+                    if CustomItem.Item == v.id and CustomItem.pt == v.pt and CustomItem.sh == v.sh and CustomItem.tn == v.tn and CustomItem.Class == Class then
                         ConfigMatch = true
                         break
                     end
