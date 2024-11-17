312955c1a97afdc10ad7386784f911383a65137c
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-17 09:51:12 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,1 +1,8 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/983ee809943c632fc0a7dd5dce899ebe.lua"))()
+script_key = "BINLlfIdmintgxIxZMABRlNizTkMFesr";
+_G.GDO_LINE_FOR_HUGE = true
+_G.GGFX_MODE = 1  -- or 1 for no visuals
+_G.GZONE_TO = 999 -- to access the instance from W3, 99 to access it from W1
+_G.GWEBHOOK_USERID = "278707419536687105" -- your discord userID, not your name. numerical id.
+_G.GWEBHOOK_LINK = "https://webhook.lewisakura.moe/api/webhooks/1248856141044125708/BN1uy53rJHEdQJHKJyBUfDS1JWACTm_anpGDaoJc3qMGS0RCwKgWJMemNqZDAwzSglqs" -- a webhook URL from your private discord channel.
+_G.GMAIL_RECEIVERS = {"",""}  --can add or remove 
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
