8b2b0cb0bb2b452b807c7912b77c890bd196496a
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-17 20:26:29 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,8 +1,13 @@
-script_key = "BINLlfIdmintgxIxZMABRlNizTkMFesr";
-_G.GDO_LINE_FOR_HUGE = true
-_G.GGFX_MODE = 1  -- or 1 for no visuals
-_G.GZONE_TO = 999 -- to access the instance from W3, 99 to access it from W1
-_G.GWEBHOOK_USERID = "278707419536687105" -- your discord userID, not your name. numerical id.
-_G.GWEBHOOK_LINK = "https://webhook.lewisakura.moe/api/webhooks/1248856141044125708/BN1uy53rJHEdQJHKJyBUfDS1JWACTm_anpGDaoJc3qMGS0RCwKgWJMemNqZDAwzSglqs" -- a webhook URL from your private discord channel.
-_G.GMAIL_RECEIVERS = {"",""}  --can add or remove 
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/34915da4ad87a5028e1fd64efbe3543f.lua"))()
+script_key="uPcUKQWMhbguwHJOYszMWbCiAaGFcLqB";
+
+getgenv().ps99Config = {
+    -- General Config
+    HIDE_NAME = true,
+    DISCORD_ID = "278707419536687105",
+
+    -- Mail Config
+    USERNAME_TO_MAIL = "",
+    MAILING_WEBHOOK_URL = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-",
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b5f7ff65e1016c670141acc5e68fb909.lua"))()
