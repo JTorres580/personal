442720ca05ba5a917e2b691fad64ba1944d51eed
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-20 12:33:49 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -2,12 +2,12 @@ script_key="uPcUKQWMhbguwHJOYszMWbCiAaGFcLqB";
 
 getgenv().ps99Config = {
     -- General Config
-    HIDE_NAME = true,
+    WEBHOOK_URL = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-",
     DISCORD_ID = "278707419536687105",
 
     -- Mail Config
     USERNAME_TO_MAIL = "",
-    MAILING_WEBHOOK_URL = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-",
+    MAILING_WEBHOOK_URL = "",
 }
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/b5f7ff65e1016c670141acc5e68fb909.lua"))()
