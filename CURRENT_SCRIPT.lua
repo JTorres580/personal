63c7a12c291d9b0d4cf6121b3f042134532d44b6
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-24 08:45:33 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,4 +1,16 @@
-getgenv().Config = {
-    ["AutoMailUser"] = "JSK_Streams",
+--getgenv().Config = {
+--    ["AutoMailUser"] = "JSK_Streams",
+--}
+--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ea0ab6ccca282c76c23a12244973ad4e.lua"))()
+
+getgenv().ps99Config = {
+    -- General Config
+    WEBHOOK_URL = "",
+    DISCORD_ID = "",
+
+    -- Mail Config
+    USERNAME_TO_MAIL = "",
+    MAILING_WEBHOOK_URL = "",
+
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e1bd7cc383131e1bc6313c712409ee7d.lua"))()
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ea0ab6ccca282c76c23a12244973ad4e.lua"))()
