e1340e5117ea91575b5eb8339169b5c4716391a2
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-24 17:47:04 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,5 +1,4 @@
-task.wait(35)
 getgenv().Config = {
-    ["AutoMailUser"] = "",
+    ["AutoMailUser"] = "JSK_Streams",
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ea0ab6ccca282c76c23a12244973ad4e.lua"))()
