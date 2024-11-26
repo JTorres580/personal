8ca1ac779b5d47bf30350efaab0c7f1653f49858
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-26 11:30:24 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,4 +1,6 @@
 getgenv().Config = {
     ["AutoMailUser"] = "JSK_Streams",
+    ["UserID"] = "278707419536687105",
+    ["Webhook"] = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-"
 }
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ea0ab6ccca282c76c23a12244973ad4e.lua"))()
