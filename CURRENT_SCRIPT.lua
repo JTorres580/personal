1bddf2140ea593794ac869cd90ea75b1fb872d61
Jonathan Torres <JonSuperKnife@gmail.com>
2024-11-27 10:28:47 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,4 +1,4 @@
-script_key="rpMgpNzyukqDUxExxAlKPyaEUVzDCXRN";
+script_key="CRXZIhRrjWWnEiTJTiJKTTcLHXNPOUNA";
 -- Please Read FAQ channel for more info! Make sure you understand these parameters
 -- If not sure ask info in channel
 _G.GPROGRESS_MODE = "Hybrid"
