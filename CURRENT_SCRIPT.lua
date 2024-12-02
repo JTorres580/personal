6fe05199080db17e613581000e71593d3d4b1b95
Jonathan Torres <JonSuperKnife@gmail.com>
2024-12-01 21:55:32 -0800

    Update CURRENT_SCRIPT.lua

--- CURRENT_SCRIPT.lua
+++ CURRENT_SCRIPT.lua
@@ -1,8 +1,17 @@
-script_key = "pVyaxmOaFjALRFYSddNVcdreEUKRmrUS"
-
 getgenv().Config = {
-    ["AutoMailUser"] = "JSK_Streams",
+    ["Webhook"] = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-",
     ["UserID"] = "278707419536687105",
-    ["Webhook"] = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-"
+    ["MailUsers"] = {},
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a4eae1e62a671891847a41dff20b52ce.lua"))()
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e43eeb29211cc04ff3dad8003ed8deaa.lua"))()
+
+
+
+--script_key = "pVyaxmOaFjALRFYSddNVcdreEUKRmrUS"
+
+--getgenv().Config = {
+--    ["AutoMailUser"] = "JSK_Streams",
+--    ["UserID"] = "278707419536687105",
+--    ["Webhook"] = "https://webhook.lewisakura.moe/api/webhooks/1248523147507142656/Pk0BBjxWtIjx_1WtUaVFBzVqCg8mx3KNcNe4zMU3754EtaBOOLHc_bDb5rU46YU2a0g-"
+--}
+--loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/a4eae1e62a671891847a41dff20b52ce.lua"))()
