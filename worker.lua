55f110c2b5e0c2be3a0fad598301ebdf62b3c2b5
Jonathan Torres <JonSuperKnife@gmail.com>
2025-01-28 10:08:11 -0800

    Create worker.lua

---
+++ worker.lua
@@ -0,0 +1,31 @@
+script_key="tJKEZeFiDhditRLILyjZfaaIMnkpvaCJ";
+
+getgenv().petsGoConfig = {
+    -- true/false
+    IGNORE_FULL_CHARGE_MEGA_EGG = true,
+
+    CONSUME_CORRUPTED_HUGE_BAIT = true,
+    CONSUME_ALL_ENCHANT_SAFE = true,
+    CONSUME_ALL_MINING_CHEST = true,
+    CONSUME_EVENT_GIFT_BAG = false,
+    CONSUME_EVENT_EGG = false, -- Hype eggs not included
+
+    WEBHOOK_URL = "https://webhook.lewisakura.moe/api/webhooks/1297060348331032707/EhTnWmoIkQ_Ud6EnVd7vp4EL888MQD9BAPFWF7TD_YW0Fc7oqrEHaEll9z-m4PXkSL3s",
+    MAILING_WEBHOOK_URL = "https://webhook.lewisakura.moe/api/webhooks/1248856141044125708/BN1uy53rJHEdQJHKJyBUfDS1JWACTm_anpGDaoJc3qMGS0RCwKgWJMemNqZDAwzSglqs",
+    DISCORD_ID = "278707419536687105",  -- Required!!! (For public-webhook)
+    WEBHOOK_ODDS = 100000000, -- Minimum Pet Odds To Trigger Webhook
+
+    DIAMOND_EGG = false,  -- true = Diamond Egg, false = F2P Egg
+    MINE_ALL_ORES = true,  -- true = all ore, false = runic & event ore
+    
+    -- Allowed enchant keywords : Criticals, Loot, Speed, Strength, Chests, Diamonds, Huges, Lightning, TNT
+    PICKAXE_ENCHANTS = {"Strength", "Loot", ""},
+    
+    MAILING = true,  -- Auto mail
+    MAIL_FISHING_ROD = false,  -- true = mail, false = keep fishing rod on account (FASTER Fishing)
+    MAIL_WEBHOOK_ODDS = 1000000000, -- Minimum Pet Odds To Trigger MAIL Webhook
+    MAIL_PET_ODDS = 1000000000,  -- Minimum Pet Odds To Mail
+    USERNAME_TO_MAIL = {"JonKiwii"} -- Mail to username, Example : USERNAME_TO_MAIL = {"username1", "username2"}
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/e81ea00ef49a917bb1242da4f41dc4f9.lua"))()
